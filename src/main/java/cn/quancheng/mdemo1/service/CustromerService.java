5af21fc2f9b4715269f6d06d55e5d23cba9ea71a
czz <2851411231@qq.com>
2018-04-27 17:48:32 +0800

    编写service层

---
+++ src/main/java/cn/quancheng/mdemo1/service/CustromerService.java
@@ -0,0 +1,46 @@
+package cn.quancheng.mdemo1.service;
+
+import cn.quancheng.mdemo1.model.Customer;
+
+import java.util.List;
+import java.util.Map;
+
+public class CustromerService {
+
+    /**
+     * 获取客户列表
+     * @param keyWrod
+     * @return
+     */
+    public List<Customer> getCustomerList(String keyWrod){
+        return null;
+    }
+
+    /**
+     * 获取客户
+     * @param id
+     * @return
+     */
+    public Customer getCustomer(int id){
+        return null;
+    }
+
+    /**
+     * 创建客户
+     * @param filedMap
+     * @return
+     */
+    public boolean creatCustomer(Map<String, Object> filedMap){
+        return false;
+    }
+
+    /**
+     * 删除客户
+     * @param id
+     * @return
+     */
+    public boolean deleteCustomer(int id){
+
+        return false;
+    }
+}
